7739ec2d630f49e67b13375ca89e183242d27acc
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-04 03:03:38 -0400

    [Neeraj] Completed Exercise by using static data in the class

--- Day4ClassExercise/OptionsViewController.swift
+++ Day4ClassExercise/OptionsViewController.swift
@@ -9,14 +9,39 @@
 import UIKit
 
 class OptionsViewController: UIViewController {
-
+ 
+    @IBOutlet weak var btnUpper: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.navigationItem.hidesBackButton = true
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func btnUpperCase(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        ViewController.option = 1
+        
+    }
 
+    @IBAction func btnLowerCase(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        ViewController.option = 2
+
+    }
+    
+    @IBAction func btnRedFont(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        ViewController.option = 3
+    }
+    
+    
+    @IBAction func btnBlueBG(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+        ViewController.option = 4
+    }
+    
     /*
     // MARK: - Navigation
 
--- Day4ClassExercise/ViewController.swift
+++ Day4ClassExercise/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    static var option = Int()
+    
+    @IBOutlet weak var txtEnterInput: UITextField!
+    @IBOutlet weak var lblShowResult: UILabel!
+    @IBOutlet weak var btnPerform: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        switch ViewController.option {
+        case 0:
+            self.lblShowResult.text = "Result"
+        case 1:
+            self.lblShowResult.text = self.txtEnterInput.text?.uppercased()
+        case 2:
+            self.lblShowResult.text = self.txtEnterInput.text?.lowercased()
+        case 3:
+            self.lblShowResult.text = self.txtEnterInput.text!
+            self.lblShowResult.textColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        case 4:
+            self.lblShowResult.text = self.txtEnterInput.text!
+            self.lblShowResult.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+            
+        default:
+            self.lblShowResult.text = "Result"
+        }
+        
+    }
+    
 }
-
